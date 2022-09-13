3fe388c76f0ca66ff59fcef698b498ed813f6a6e
pekka-kanerva2 <113357959+pekka-kanerva2@users.noreply.github.com>
2022-09-13 15:43:46 +0300

    added df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Kane''s Diner')
 streamlit.header('Herkku-Breakfast')
@@ -11,3 +14,5 @@ streamlit.text('🥑🍞 Avocado-Paahtis')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
