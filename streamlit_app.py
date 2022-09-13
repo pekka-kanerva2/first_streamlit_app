673dfd65cfa296af648038d71d1b0a482056ff0e
pekka-kanerva2 <113357959+pekka-kanerva2@users.noreply.github.com>
2022-09-13 09:02:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('🥗 Pinatti-smoothie')
 streamlit.text('🐔 Keitetty kananmuna')
 streamlit.text('🥑🍞 Avocado-Paahtis')
 
-streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
-streamlit.text('🍟')
+# streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
