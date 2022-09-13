841ad95c14a4aa44907af275546221d9c9ba3f23
pekka-kanerva2 <113357959+pekka-kanerva2@users.noreply.github.com>
2022-09-13 08:54:22 +0300

    emojitest

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,6 @@ streamlit.text('🥗 Pinatti-smoothie')
 streamlit.text('🐔 Keitetty kananmuna')
 streamlit.text('🥑🍞 Avocado-Paahtis')
 
+streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
