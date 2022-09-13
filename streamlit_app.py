0dae44056adda791614f46e1d3842f492d0f807c
pekka-kanerva2 <113357959+pekka-kanerva2@users.noreply.github.com>
2022-09-13 15:50:03 +0300

    index by name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Kane''s Diner')
 streamlit.header('Herkku-Breakfast')
@@ -12,8 +11,10 @@ streamlit.text('🥑🍞 Avocado-Paahtis')
 
 # streamlit.text('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
