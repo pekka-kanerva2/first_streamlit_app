caffc578f062fbf8718da0723e18846f4d74c3b2
pekka-kanerva2 <113357959+pekka-kanerva2@users.noreply.github.com>
2022-09-14 16:39:21 +0300

    reorg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('Pekan Diner')
@@ -29,7 +32,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # put json to df
@@ -37,8 +39,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write df
 streamlit.dataframe(fruityvice_normalized)
 
+# xxxxxxxxxxxxxxxxxxxxxxxx
+streamlit.stop()
+
 # Snowflake
-import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
